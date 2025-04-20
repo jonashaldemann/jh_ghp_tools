06089e3511ddca364cbb19221eae9105d6d37b20
jonashaldemann <jonasha@users.noreply.github.com>
2025-04-20 13:14:44 +0200

    bake instant_huesli

--- src/jh_ghp_tools/instant_huesli.py
+++ src/jh_ghp_tools/instant_huesli.py
@@ -1,44 +1,44 @@
 import ghpythonlib.components as gh
 import math
+import scriptcontext as sc
+import Rhino
 
 def instant_huesli(curve, giebelhoehe, flip, giebelposition, dachwinkel, bake):
 
-    if flip:
-        angle = 0.5 * math.pi
-    else:
-        angle = 0.0
+    angle = 0.5 * math.pi if flip else 0.0
 
     # Solid
     extr_vec = gh.UnitZ(giebelhoehe)
-    extrusion = gh.Extrude(curve, extr_vec)
-    solid = gh.CapHoles(extrusion)
+    solid = gh.CapHoles(gh.Extrude(curve, extr_vec))
 
     # Vertikale Plane unter dem Giebel
     segments, _ = gh.Explode(curve, False)
-    lengths = gh.Length(segments)
-    longest_segment_index = lengths.index(max(lengths))
-    longest_segment = segments[longest_segment_index]
+    longest_segment = max(segments, key=lambda seg: gh.Length(seg))
     start, end = gh.EndPoints(longest_segment)
-    vector, _ = gh.Vector2Pt(start, end, False)
-    vector, _ =  gh.Rotate(vector, angle, gh.XYPlane(gh.ConstructPoint(0.0, 0.0, 0.0)))
+    vector, _ = gh.Rotate(gh.Vector2Pt(start, end, False), angle, gh.XYPlane(gh.ConstructPoint(0.0, 0.0, 0.0)))
     plane, _ = gh.AlignPlane(gh.XYPlane(gh.ConstructPoint(0.0, 0.0, 0.0)), vector)
     bbox, _ = gh.BoundingBox(solid, plane)
-    start, end = gh.BoxCorners(bbox)[4], gh.BoxCorners(bbox)[6]
-    diagonal = gh.Line(start, end)
+    diagonal = gh.Line(gh.BoxCorners(bbox)[4], gh.BoxCorners(bbox)[6])
     giebelpunkt, _, _ = gh.EvaluateLength(diagonal, giebelposition, True)
     giebelplane = gh.ConstructPlane(giebelpunkt, vector, plane.ZAxis)
 
     # Dachschräge
-    dach_plane1, _ = gh.Rotate3D(giebelplane, math.radians(90 - dachwinkel), giebelpunkt, vector)
-    dach_plane2, _ = gh.Mirror(dach_plane1, giebelplane)
-    dach_planes = [dach_plane1, dach_plane2]
+    dach_planes = [
+        gh.Rotate3D(giebelplane, math.radians(90 - dachwinkel), giebelpunkt, vector)[0],
+        gh.Mirror(gh.Rotate3D(giebelplane, math.radians(90 - dachwinkel), giebelpunkt, vector)[0], giebelplane)[0]
+    ]
 
     # Schneiden
     for plane in dach_planes:
         crv, _ = gh.BrepXPlane(solid, plane)
-        breps = gh.SplitBrep(solid, crv)
-        breps = gh.CapHoles(breps)
-        volumes, _ = gh.Volume(breps)
-        solid = breps[volumes.index(max(volumes))]
+        breps = gh.CapHoles(gh.SplitBrep(solid, crv))
+        solid = max(breps, key=lambda brep: gh.Volume(brep)[0])
+
+    # Baken
+    if bake:
+        sc.doc = Rhino.RhinoDoc.ActiveDoc
+        attributes = Rhino.DocObjects.ObjectAttributes()
+        sc.doc.Objects.AddBrep(solid, attributes)
+        sc.doc = sc.sticky.get("ghdoc", None)  # Retrieve ghdoc from scriptcontext's sticky dictionary
 
     return solid
