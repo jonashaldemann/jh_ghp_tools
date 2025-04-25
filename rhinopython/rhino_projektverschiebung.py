03ac241fea49c169728e24ac455f3c0a32ac7824
jonashaldemann <jonasha@users.noreply.github.com>
2025-04-25 17:20:06 +0200

    rotation to rhino projektverschiebung

--- rhinopython/rhino_projektverschiebung.py
+++ rhinopython/rhino_projektverschiebung.py
@@ -21,11 +21,21 @@ geometrie = rs.GetObjects(message="Waehl Geometrien aus", filter=0)
 
 file_path = get_file_path()
 if file_path:
-    coordinates = get_coordinates_from_file(file_path)
-    x, y, z = coordinates  # Unpack coordinates for calculations
-    
-    # Determine z movement based on user input
-    include_z = toggle_z_movement()
-    z_movement = -z if include_z else 0
-    
-    rs.MoveObjects(geometrie, (-x, -y, z_movement))
+    with open(file_path, 'r') as file:
+        # Read the first line for coordinates
+        line = file.readline().strip()
+        coordinates = tuple(map(float, line.split(',')))
+        x, y, z = coordinates  # Unpack coordinates for calculations
+        
+        # Read the second line for rotation angle
+        rotation_line = file.readline().strip()
+        rotation_angle = float(rotation_line) if rotation_line else None
+        
+        # Determine z movement based on user input
+        include_z = toggle_z_movement()
+        z_movement = -z if include_z else 0
+        
+        rs.MoveObjects(geometrie, (-x, -y, z_movement))
+        if rotation_angle is not None:
+            origin = (0, 0, 0)
+            rs.RotateObjects(geometrie, origin, rotation_angle)
